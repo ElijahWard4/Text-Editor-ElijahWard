bd8fb743ff3d223bba615e0fa6f8f64e63e13bd7
EliWard4 <elijah.ward014@gmail.com>
2024-08-08 01:34:38 -0500

    method that accepts some content and adds it to the database and one gets all the content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  //database name, version
+  const jateDb = await openDB('jate', 1);
+  //transaction specifying database and type of read/write
+  const tx = jateDb.transaction('jate', 'readwrite');
+  //object store
+  const store = tx.objectStore('jate');
+  //put request
+  const request = store.put({id: 1, value: content});
+  return request;
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  //database name, version
+  const jateDb = await openDB('jate', 1);
+  //readonly
+  const tx = jateDb.transaction('jate', 'readonly');
+  //object store
+  const store = tx.objectStore('jate');
+  //get all request
+  const request = store.getAll();
+  //wait for the request to finish and return the value
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+}
 
 initdb();
